b6f87bc707e78750f475561dbd72f228db20a1ae
pavulis97 <pavulis97@gmail.com>
2023-10-03 16:53:38 +0300

    First build

---
+++ vscode-build/obj/gecko_sdk_4.3.1/platform/driver/button/src/sl_button.d
@@ -0,0 +1,34 @@
+vscode-build/obj/gecko_sdk_4.3.1/platform/driver/button/src/sl_button.o: \
+ gecko_sdk_4.3.1/platform/driver/button/src/sl_button.c \
+ gecko_sdk_4.3.1/platform/common/toolchain/inc/sl_gcc_preinclude.h \
+ gecko_sdk_4.3.1/platform/driver/button/inc/sl_button.h \
+ gecko_sdk_4.3.1/platform/common/inc/sl_common.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/stdint.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/features.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/_newlib_version.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_stdint.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ gecko_sdk_4.3.1/platform/common/inc/sl_assert.h \
+ gecko_sdk_4.3.1/platform/CMSIS/Core/Include/cmsis_compiler.h \
+ gecko_sdk_4.3.1/platform/CMSIS/Core/Include/cmsis_gcc.h \
+ gecko_sdk_4.3.1/platform/common/inc/sl_status.h \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h
+gecko_sdk_4.3.1/platform/common/toolchain/inc/sl_gcc_preinclude.h:
+gecko_sdk_4.3.1/platform/driver/button/inc/sl_button.h:
+gecko_sdk_4.3.1/platform/common/inc/sl_common.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/stdint.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/machine/_default_types.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/features.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/_newlib_version.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_intsup.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/arm-none-eabi/include/sys/_stdint.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h:
+gecko_sdk_4.3.1/platform/common/inc/sl_assert.h:
+gecko_sdk_4.3.1/platform/CMSIS/Core/Include/cmsis_compiler.h:
+gecko_sdk_4.3.1/platform/CMSIS/Core/Include/cmsis_gcc.h:
+gecko_sdk_4.3.1/platform/common/inc/sl_status.h:
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/toolchains/gnu_arm/10.3_2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
